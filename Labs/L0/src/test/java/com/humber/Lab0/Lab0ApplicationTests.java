80acd9687a0f82c621dc3b306f8f440b1fc864eb
Niki9526 <n01591561@humber.ca>
2024-05-10 13:44:31 -0400

    first commit new project

---
+++ Labs/L0/src/test/java/com/humber/Lab0/Lab0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.Lab0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
