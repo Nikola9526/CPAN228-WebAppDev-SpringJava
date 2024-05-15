a5e05ad3870c0b97e6b1878b60652d0c4f1b158b
Niki9526 <n01591561@humber.ca>
2024-05-15 19:50:43 -0400

    added dished list to add dishes, added method to return all dished

--- Week2ThymeLeafApp/src/main/java/com/humber/Week2ThymeLeafApp/repositories/DishRepository.java
+++ Week2ThymeLeafApp/src/main/java/com/humber/Week2ThymeLeafApp/repositories/DishRepository.java
@@ -1,19 +1,49 @@
 package com.humber.Week2ThymeLeafApp.repositories;
 // importing Model Package Dish
 import com.humber.Week2ThymeLeafApp.models.Dish;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DishRepository {
 
+    //@Getter
     private static List<Dish> dishes = new ArrayList<>();
 
     static {
-        //dishes.add(new Dish(1,"p","k",1.2)); // makeing dish object
+        //dishes.add(new Dish(1,"","",1.2)); // making dish object 1st way
         dishes.add(
                 Dish.builder().id(1).name("Burger").category("Non-Veg").price(5.00)
                         .build()
+                // we use builder pattern shows what the field is named
+
+        );
+        dishes.add(
+                Dish.builder().id(2).name("Pizza").category("Sausage").price(15.00)
+                        .build()
+                // we use builder pattern shows what the field is named
+
+        );
+        dishes.add(
+                Dish.builder().id(3).name("Poutine").category("Fires,Cheese, Gravy").price(3.00)
+                        .build()
+                // we use builder pattern shows what the field is named
+
+        );
+        dishes.add(
+                Dish.builder().id(4).name("Meatballs").category("Meat").price(25.00)
+                        .build()
+                // we use builder pattern shows what the field is named
+
         );
     }//static
+
+    //add method that returns
+
+
+    public static List<Dish> getDishes() {
+        return dishes;
+    }
 }// class
+//repo done next service
