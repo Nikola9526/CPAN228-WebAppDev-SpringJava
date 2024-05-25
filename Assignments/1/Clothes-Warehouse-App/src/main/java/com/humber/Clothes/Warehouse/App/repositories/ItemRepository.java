32dd8b73f04e873e94f829e2caac14b09c5aee00
Niki9526 <n01591561@humber.ca>
2024-05-25 13:10:18 -0400

    Initial Commit, created  Item Repo Class

---
+++ Assignments/1/Clothes-Warehouse-App/src/main/java/com/humber/Clothes/Warehouse/App/repositories/ItemRepository.java
@@ -0,0 +1,34 @@
+package com.humber.Clothes.Warehouse.App.repositories;
+
+import com.humber.Clothes.Warehouse.App.models.Brand;
+import com.humber.Clothes.Warehouse.App.models.Item;
+
+import java.util.*;
+
+
+public class ItemRepository {
+    private static List<Item> items = new ArrayList<>();
+    //creating dummy data for the  warehouse
+    static {
+        items.add(
+                Item.builder().id(1).name("Summer Dress").brand(Brand.StoneIsland).
+                         year_creation(1990)
+                        .price(20.00).build()
+        );
+
+        items.add(
+                Item.builder().id(2).name("Skinny Jeans").brand(Brand.Guess).year_creation(2021)
+                        .price(40.00).build()
+        );
+
+        items.add(
+                Item.builder().id(3).name("Velour Track Suit").brand(Brand.Balenciaga).year_creation(2024)
+                        .price(90.00).build()
+        );
+    }
+    // return method
+    public static List<Item> getItems() {
+        return items;
+    }
+
+}
