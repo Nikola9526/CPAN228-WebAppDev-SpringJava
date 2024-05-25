a70a2365c127de612ae26f86dbef4cd5f2ef8354
Niki9526 <n01591561@humber.ca>
2024-05-25 13:09:58 -0400

    Initial Commit, created service class

---
+++ Assignments/1/Clothes-Warehouse-App/src/main/java/com/humber/Clothes/Warehouse/App/services/ItemService.java
@@ -0,0 +1,17 @@
+package com.humber.Clothes.Warehouse.App.services;
+
+import com.humber.Clothes.Warehouse.App.models.Item;
+import com.humber.Clothes.Warehouse.App.repositories.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
+public class ItemService {
+
+    //return  item repo class
+    public List<Item> getAllItems(){
+        return ItemRepository.getItems();
+    }
+
+}
