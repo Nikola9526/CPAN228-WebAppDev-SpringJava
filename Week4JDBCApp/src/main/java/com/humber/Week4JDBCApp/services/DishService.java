ace7c16176a08b5d1a5e10c3cc378578d28957fd
Niki9526 <n01591561@humber.ca>
2024-05-29 20:10:02 -0400

    added TO DISH SERVICE CLASS

--- Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/services/DishService.java
+++ Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/services/DishService.java
@@ -2,15 +2,29 @@ package com.humber.Week4JDBCApp.services;
 
 import com.humber.Week4JDBCApp.models.Dish;
 import com.humber.Week4JDBCApp.repositories.DishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class DishService {
     // Add a method to get all dishes
-    /*public List<Dish> getAllDishes() {
-        // get
-        return DishRepository.getDishes();
-    }*/
+    // business logic
+
+    @Autowired
+    private DishRepository dishRepository; // making object
+
+    // need to inject DIshRepo Class but can not make object so use @Autowire for injection of repo class
+    // get  All Dishes
+    public List<Dish> getAllDishes() {
+        return dishRepository.getDishes();
+        //return DishRepository.getDishes();
+            //throwing error cause it is called as static, but it is not static
+    }
+
+    /*save a dish*/
+    public int saveDish (Dish dish){
+        return dishRepository.save(dish);
+    }
 
 }//Class
