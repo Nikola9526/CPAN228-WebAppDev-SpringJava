5b9c74125eacacbc4a8d6027b5381d0c2cbefce1
Niki9526 <n01591561@humber.ca>
2024-05-30 00:15:20 -0400

    fixed spacing

--- Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
+++ Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
@@ -10,28 +10,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-@Repository // need to add to make class a BEAN // or @Autowire will not work
+@Repository //need to add to make class a BEAN //or @Autowire will not work
 public class DishRepository {
     @Autowired
 
     JdbcTemplate template; // inject this by adding @Autowire
-    // save a dish
 
+    //save a dish
     public int save(Dish dish) { //
-        String sql = "INSERT INTO dishes (name, description, price) VALUES (?, ?, ?)";
-       return  template.update(sql, dish.getName(), dish.getCategory(), dish.getPrice() );       // to make changes
+        String sql = "INSERT INTO dish (name, category, price) VALUES (?, ?, ?)";
+       return  template.update(sql, dish.getName(), dish.getCategory(), dish.getPrice() ); // to make changes
         // return int
     }
 
-
-
-
-
-
-
-    // get all dishes // data is at DB
+    /*get all dishes //data is at DB */
     public  List <Dish> getDishes (){
-        String sql = "SELECT * FROM dishes";
+        String sql = "SELECT * FROM dish";
 
         RowMapper<Dish> mapper = new RowMapper<Dish>() {
 
@@ -39,20 +33,18 @@ public class DishRepository {
             public Dish mapRow(ResultSet rs, int rowNum) throws SQLException {
                 Dish mydish = new Dish();
                 // mapRow takes out data from table
-                mydish.setId(rs.getInt(1)); // or this mydish.setId(rs.getInt("id"));
+                mydish.setId(rs.getInt(1)); //or this mydish.setId(rs.getInt("id"));
                 mydish.setName(rs.getString(2));
                 mydish.setCategory(rs.getString(3));
                 mydish.setPrice(rs.getDouble(4));
-
-
 
                 return mydish;
             }
         };
 
-         List <Dish> dishes = template.query(sql, mapper); // do not have to add List<Dish> just return template.query(sql, mapper)
+         List <Dish> dishes = template.query(sql, mapper); //do not have to add List<Dish> just return template.query(sql, mapper)
          return dishes;
-         // template will hold list of dishes
+         //template will hold list of dishes
     }
 
 
