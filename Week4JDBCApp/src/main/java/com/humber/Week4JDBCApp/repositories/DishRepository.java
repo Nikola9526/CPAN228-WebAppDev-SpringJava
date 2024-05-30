41256d300bf27ac0ed9810388f7cfc34f095a3ab
Niki9526 <n01591561@humber.ca>
2024-05-29 20:10:47 -0400

    added save a dish in repo and get dishes in DB

--- Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
+++ Week4JDBCApp/src/main/java/com/humber/Week4JDBCApp/repositories/DishRepository.java
@@ -3,7 +3,7 @@ package com.humber.Week4JDBCApp.repositories;
 import com.humber.Week4JDBCApp.models.Dish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.RowMapper; // rowmapper from spring frame work
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -17,8 +17,19 @@ public class DishRepository {
     JdbcTemplate template; // inject this by adding @Autowire
     // save a dish
 
-    // get all dishes
+    public int save(Dish dish) { //
+        String sql = "INSERT INTO dishes (name, description, price) VALUES (?, ?, ?)";
+       return  template.update(sql, dish.getName(), dish.getCategory(), dish.getPrice() );       // to make changes
+        // return int
+    }
 
+
+
+
+
+
+
+    // get all dishes // data is at DB
     public  List <Dish> getDishes (){
         String sql = "SELECT * FROM dishes";
 
@@ -26,11 +37,22 @@ public class DishRepository {
 
             @Override
             public Dish mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return null;
+                Dish mydish = new Dish();
+                // mapRow takes out data from table
+                mydish.setId(rs.getInt(1)); // or this mydish.setId(rs.getInt("id"));
+                mydish.setName(rs.getString(2));
+                mydish.setCategory(rs.getString(3));
+                mydish.setPrice(rs.getDouble(4));
+
+
+
+                return mydish;
             }
         };
 
-         template.query(sql, mapper);
+         List <Dish> dishes = template.query(sql, mapper); // do not have to add List<Dish> just return template.query(sql, mapper)
+         return dishes;
+         // template will hold list of dishes
     }
 
 
