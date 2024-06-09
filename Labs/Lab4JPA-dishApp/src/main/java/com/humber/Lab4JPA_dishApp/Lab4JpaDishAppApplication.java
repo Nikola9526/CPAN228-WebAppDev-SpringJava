7606ed79e6165aac07b116fc48986d6206b866a3
Niki9526 <n01591561@humber.ca>
2024-06-09 01:12:21 -0400

    new inital commit

---
+++ Labs/Lab4JPA-dishApp/src/main/java/com/humber/Lab4JPA_dishApp/Lab4JpaDishAppApplication.java
@@ -0,0 +1,13 @@
+package com.humber.Lab4JPA_dishApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4JpaDishAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4JpaDishAppApplication.class, args);
+	}
+
+}
---
+++ Labs/Lab4JPA-dishApp/src/test/java/com/humber/Lab4JPA_dishApp/Lab4JpaDishAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.Lab4JPA_dishApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4JpaDishAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
