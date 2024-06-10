d2f2b7482d48d3b5f7b7a926f18f6c0e2a2d046d
Niki9526 <n01591561@humber.ca>
2024-06-09 22:38:07 -0400

    added  ; semicolon to end caused error

--- Labs/Lab4JPA-dishApp/src/main/resources/data.sql
+++ Labs/Lab4JPA-dishApp/src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO dish (name, category, price)
 VALUES
     ('CHEESE BURGER', 'Fast Food', 12),
     ('MAC AND CHEESE', 'COMFORT FOOD', 8),
-    ('Pizza', 'Fast Food', 5),
+    ('Pizza', 'Fast Food', 5);
