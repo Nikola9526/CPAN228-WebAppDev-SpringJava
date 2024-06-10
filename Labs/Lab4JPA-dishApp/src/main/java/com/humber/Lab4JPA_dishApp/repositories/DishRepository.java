7db0a2d017249d8b0b6c340920d5fe764f57ad40
Niki9526 <n01591561@humber.ca>
2024-06-10 01:30:36 -0400

    added find  by catagory and price

--- Labs/Lab4JPA-dishApp/src/main/java/com/humber/Lab4JPA_dishApp/repositories/DishRepository.java
+++ Labs/Lab4JPA-dishApp/src/main/java/com/humber/Lab4JPA_dishApp/repositories/DishRepository.java
@@ -3,7 +3,11 @@ package com.humber.Lab4JPA_dishApp.repositories;
 import com.humber.Lab4JPA_dishApp.models.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface  DishRepository extends JpaRepository<Dish, Integer> {
+
+    List<Dish> findByCategoryAndPrice(String searchCategory, Double searchPrice);
 
 
 }
