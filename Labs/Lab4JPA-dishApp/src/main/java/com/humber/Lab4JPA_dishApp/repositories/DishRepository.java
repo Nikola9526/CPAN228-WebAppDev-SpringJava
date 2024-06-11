8d32399508a0b0920cc4971419093cb4a2de05cb
Niki9526 <n01591561@humber.ca>
2024-06-11 12:03:06 -0400

    added comments, @Repo

--- Labs/Lab4JPA-dishApp/src/main/java/com/humber/Lab4JPA_dishApp/repositories/DishRepository.java
+++ Labs/Lab4JPA-dishApp/src/main/java/com/humber/Lab4JPA_dishApp/repositories/DishRepository.java
@@ -2,12 +2,14 @@ package com.humber.Lab4JPA_dishApp.repositories;
 
 import com.humber.Lab4JPA_dishApp.models.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface  DishRepository extends JpaRepository<Dish, Integer> {
 
     List<Dish> findByCategoryAndPrice(String searchCategory, Double searchPrice);
+    // anything extending from jpa is already a component so no need for @Repository
 
 
 }
