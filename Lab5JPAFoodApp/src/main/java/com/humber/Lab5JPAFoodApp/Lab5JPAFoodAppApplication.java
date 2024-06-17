dd58e71c2d533a7aea02ecb425afe6d16a88f1e6
Niki9526 <n01591561@humber.ca>
2024-06-17 14:20:18 -0400

    changed to Lab5

--- Lab5JPAFoodApp/src/main/java/com/humber/Lab5JPAFoodApp/Lab5JPAFoodAppApplication.java
+++ Lab5JPAFoodApp/src/main/java/com/humber/Lab5JPAFoodApp/Lab5JPAFoodAppApplication.java
@@ -1,25 +1,25 @@
-package com.humber.Week6JPAFoodApp;
+package com.humber.Lab5JPAFoodApp;
 
-import com.humber.Week6JPAFoodApp.models.Dish;
-import com.humber.Week6JPAFoodApp.services.DishService;
+import com.humber.Lab5JPAFoodApp.models.Dish;
+import com.humber.Lab5JPAFoodApp.services.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week6JPAFoodAppApplication implements CommandLineRunner {
+public class Lab5JPAFoodAppApplication implements CommandLineRunner {
 
 	@Autowired
 	// constructor injection // making object of dish service class
 	private final DishService dishService;
 
-	public Week6JPAFoodAppApplication(DishService dishService) {
+	public Lab5JPAFoodAppApplication(DishService dishService) {
 		this.dishService = dishService;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Week6JPAFoodAppApplication.class, args);
+		SpringApplication.run(Lab5JPAFoodAppApplication.class, args);
 	}
 
 
@@ -27,7 +27,7 @@ public class Week6JPAFoodAppApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// anything here will excuite when app is run
 
-		System.out.println("Hello from CLR!");
+		System.out.println("Hello from CLR! \n Add Data to Database!");
 		dishService.saveDish(new Dish(1, "Pizza", "Non-Veg", 12.0));
 		dishService.saveDish(new Dish(2, "Burger", "Non-Veg", 8.5));
 		dishService.saveDish(new Dish(3, "Pasta", "Veg", 10.0));
