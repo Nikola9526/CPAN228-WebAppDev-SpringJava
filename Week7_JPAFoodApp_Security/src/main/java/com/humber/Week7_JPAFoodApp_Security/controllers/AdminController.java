2d5a0cfe2ba307021201ec1ebcd516341e372b07
Niki9526 <n01591561@humber.ca>
2024-06-19 18:50:33 -0400

    create admin controller class copyed update, delete, add enpoints

---
+++ Week7_JPAFoodApp_Security/src/main/java/com/humber/Week7_JPAFoodApp_Security/controllers/AdminController.java
@@ -0,0 +1,81 @@
+package com.humber.Week7_JPAFoodApp_Security.controllers;
+
+import com.humber.Week7_JPAFoodApp_Security.models.Dish;
+import com.humber.Week7_JPAFoodApp_Security.services.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller// Auto Bean //so spring  knows it is controller
+@RequestMapping("/restaurant/admin")
+public class AdminController {
+
+    private final DishService dishService;
+
+    //constructor Injection
+    @Autowired
+    public AdminController(DishService dishService) {
+        this.dishService= dishService;
+    }
+
+    @Value("${restaurant.name}")
+    private String name;
+// only 5 endpoints need
+
+    // add a method
+    //GET /restaurant/add-dish
+    @GetMapping("/add-dish") // Add a Dish Page "View- add-dish"
+    public String addDish(Model model){
+        model.addAttribute("dish", new Dish());
+        return "add-dish";
+        // it is GET it just opens the form //one get to open page //one post to post data
+    }
+
+    //save the dish
+    //POST
+    @PostMapping("/add-dish")
+    public String addDish(@ModelAttribute Dish dish, Model model){
+        // saving in db
+        int statusCode = dishService.saveDish(dish);
+        // 1 for success, 0 for fail
+        if (statusCode == 1){
+            return "redirect:/restaurant/menu/1?message=dish added successfully!"; // so you do not need to repeat code
+        } // else
+        return "redirect:/restaurant/menu/1?errormessage=dish not added! Price is More Than 20!";
+    }
+
+    // delete a dish // need to change to GET cause html does not understand DELETE
+    //@DeleteMapping("/delete/{id}") // this is path variable in {id} in curly braces
+    @GetMapping("/delete/{id}") // this is path variable in {id} in curly braces
+    public String deleteDish(@PathVariable int id){
+        int deleteStatusCode = dishService.deleteDish(id);
+        if(deleteStatusCode ==1) {
+            return "redirect:/restaurant/menu/1?message=The Dish has been Deleted Successfully!";
+        }
+        return "redirect:/restaurant/menu/1?errormessage=Dish to be Deleted Does not exist!";
+    }
+    // update/change a dish
+    //GET  /restaurant/update/{id} // Same Add a Dish Page
+    // but with different name depending on conditions if fields have values or not
+    @GetMapping("/update/{id}")
+    public String updateDish (Model model, @PathVariable int id) {
+        Optional<Dish> dishToUpdate;
+        dishToUpdate = dishService.getDishById(id);
+
+        // showing values in form prefilled from existing data in database
+        model.addAttribute("dish", dishToUpdate.orElse(null));
+        return "add-dish";
+    }
+    //update a dish
+    @PostMapping("/update-dish")
+    public String updateDish(@ModelAttribute Dish dish) {
+        dishService.updateDish(dish);
+        return "redirect:/restaurant/menu/1?message=Dish Has Been Updated Successfully!";
+    }
+
+}
