e7e9187ef280f5410b75acf309c64c397e22250f
Niki9526 <n01591561@humber.ca>
2024-06-19 18:19:28 -0400

    initial commit, in class Week 7 created Project

---
+++ Week7_JPAFoodApp_Security/src/test/java/com/humber/Week7_JPAFoodApp_Security/Week7_JPAFoodApp_SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.Week7_JPAFoodApp_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7_JPAFoodApp_SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
