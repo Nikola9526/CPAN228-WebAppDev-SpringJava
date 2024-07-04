f1179ad3007655b7defea9d53447de620a42ac15
Niki9526 <n01591561@humber.ca>
2024-07-04 16:46:52 -0400

    injecting userservice class into here AuthController for register form and  made get/post endpoint for registering

--- Week8_JPA_SpringSecurity_Regis/src/main/java/com/humber/Week8_JPA_SpringSecurity_Regis/controllers/AuthController.java
+++ Week8_JPA_SpringSecurity_Regis/src/main/java/com/humber/Week8_JPA_SpringSecurity_Regis/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package com.humber.Week8_JPA_SpringSecurity_Regis.controllers;
 
+import com.humber.Week8_JPA_SpringSecurity_Regis.models.myUser;
+import com.humber.Week8_JPA_SpringSecurity_Regis.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,10 +10,19 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    //injecting User Service in to here //constructor injection
+    private final UserService userService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
     @Value("${restaurant.name}")
     private String name;
 //  passing Name
@@ -38,5 +49,28 @@ public class AuthController implements org.springframework.boot.web.servlet.erro
         new SecurityContextLogoutHandler().logout(request, response, authentication);
         //return "redirect:/restaurant/home?message=You have been Logged Out Successfully!";
         return "redirect:/login?message=You have been Logged Out Successfully!";
+    }
+
+    //adding registration endpoint //the form view page
+    @GetMapping("/registers")
+    public String register(Model model, @RequestParam (required = false) String message) {
+        //send empty object
+        model.addAttribute("user", new myUser());
+        model.addAttribute("message", message); //shows success or error message
+        model.addAttribute("restaurantName", name); //shows name of site
+        return "auth/register"; //the view (html)
+    }
+
+    // post endpoint to action of clicking register
+    @PostMapping("/registers")
+    public String registerPost(@ModelAttribute myUser user) {
+        // saving in db // save the user
+        int saveUserCode = userService.saveUser(user);
+        // 0 for already exsits , 1 for new user stored
+        if (saveUserCode == 0) {
+            return  "redirect:/registers?message=Username Already Exists!";
+        } else /* code == 1*/ {
+            return "redirect:/login?message=Registration Done Successfully!";
+        }
     }
 }
