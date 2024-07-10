05724f710df12347e53aae2fce8a0e533df23239
Niki9526 <n01591561@humber.ca>
2024-07-10 18:36:29 -0400

    new project created spring rest api initial commit

---
+++ Week9SpringRestAPI/src/main/java/com/humber/Week9SpringRestAPI/Week9SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.humber.Week9SpringRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9SpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week9SpringRestApiApplication.class, args);
+	}
+
+}
