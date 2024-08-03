817316c39cafb9b53397abe8bf2818b68ffc6235
Niki9526 <n01591561@humber.ca>
2024-08-03 00:11:28 -0400

    updated main class to add data dummy

--- FinalProject_userBlog/src/main/java/com/humber/FinalProject_userBlog/FinalProjectUserBlogApplication.java
+++ FinalProject_userBlog/src/main/java/com/humber/FinalProject_userBlog/FinalProjectUserBlogApplication.java
@@ -1,13 +1,29 @@
 package com.humber.FinalProject_userBlog;
 
+import com.humber.FinalProject_userBlog.models.Comment;
+import com.humber.FinalProject_userBlog.models.Post;
+import com.humber.FinalProject_userBlog.models.myUser;
+import com.humber.FinalProject_userBlog.services.PostService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalProjectUserBlogApplication {
+public class FinalProjectUserBlogApplication implements CommandLineRunner {
+
+	private final PostService postService;
+	public FinalProjectUserBlogApplication(PostService postService) {
+		this.postService = postService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectUserBlogApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		//postService.addPost(new Post(1, "Parking for Sale", "Selling a Spot for $100 Access Now", new myUser(10,"NIK", "STOJ","NIKOLA26", "STOJ@GMAIL.COM",new Post(1)), "we", 1));
+
+	}
 }
